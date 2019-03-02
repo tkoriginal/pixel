4f3d503b5537c103b5d13ca98c905e41768e0367
Tauqueer Khan <tauqueer.khan@gmail.com>
2019-03-02 16:10:13 -0500

    Completed front and backend handshake

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // An api endpoint that returns a short list of items
 app.get('/api/getList', (req,res) => {
     var list = ["item1", "item2", "item3"];
+    console.log('Sent list of items', list);
     res.json(list);
-    console.log('Sent list of items');
 });
 
 // Handles any requests that don't match the ones above
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hello There!</h1>
+    const App = () => (
+      <div>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+        </Switch>
       </div>
+    )
+    return (
+      <Switch>
+        <App/>
+      </Switch>
     );
   }
 }
