12097e08311239242d66f8d83c88eea2020aec79
Tauqueer Khan <tauqueer.khan@gmail.com>
2019-03-02 16:44:08 -0500

    Added Knex and Postgres

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,47 @@
-const express = require('express');
-const path = require('path');
+"use strict";
 
-const app = express();
+require('dotenv').config();
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'public')));
+const PORT        = process.env.PORT || 5000;
+const ENV         = process.env.ENV || "development";
+const express     = require("express");
+const bodyParser  = require("body-parser");
+const app         = express();
 
-// An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3"];
-    console.log('Sent list of items', list);
-    res.json(list);
+const knexConfig  = require("./knexfile");
+const knex        = require("knex")(knexConfig[ENV]);
+const morgan      = require('morgan');
+const knexLogger  = require('knex-logger');
+
+// Seperated Routes for each Resource
+// const usersRoutes = require("./routes/users");
+
+// Load the logger first so all (static) HTTP requests are logged to STDOUT
+// 'dev' = Concise output colored by response status for development use.
+//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+app.use(morgan('dev'));
+
+// Log knex SQL queries to STDOUT as well
+app.use(knexLogger(knex));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Mount all resource routes
+// app.use("/api/users", usersRoutes(knex));
+
+// Home page
+app.get("/", (req, res) => {
+    knex
+    .select("*")
+    .from("users")
+    .then((results) => {
+      res.json(results);
+  });
 });
 
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.listen(PORT, () => {
+  console.log("Example app listening on port " + PORT);
 });
-
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log('App is listening on port ' + port);
-
 
 
