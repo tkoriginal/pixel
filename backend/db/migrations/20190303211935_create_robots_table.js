b1cc5ba0cf4ad8e534ebb6a617f967acbedcfb27
MaxHuw <m.huw.graham@gmail.com>
2019-03-03 21:38:53 +0000

    Added robots table and seed

---
+++ backend/db/migrations/20190303211935_create_robots_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('robots', function (table) {
+    table.increments('id').primary();
+    table.string('name');
+    table.integer('user_id').references('id').on('users');
+    table.integer('hp');
+    table.integer('str');
+    table.integer('dex');
+    table.integer('arm');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('robots');
+};
--- backend/db/seeds/all-tables.js
+++ backend/db/seeds/all-tables.js
@@ -1,10 +1,20 @@
-exports.seed = function(knex, Promise) {
-  return knex('users').del()
-    .then(function () {
-      return Promise.all([
-        knex('users').insert({name: 'Alice'}),
-        knex('users').insert({name: 'Bob'}),
-        knex('users').insert({name: 'Charlie'})
-      ]);
-    });
+exports.seed = async function(knex, Promise) {
+  await knex('users').del();
+  await Promise.all (
+    [
+      knex('users').insert({name: 'Alice', password: 'alice', email: 'alice@alice.com'}),
+      knex('users').insert({name: 'Bob', password: 'bob', email: 'bob@bob.com'}),
+      knex('users').insert({name: 'Mark', password: 'mark', email: 'mark@mark.com'}),
+    ]
+  );
+
+  await knex('robots').del();
+  await Promise.all (
+    [
+      knex('robots').insert({name: 'Robot1', user_id: 1, hp: 10, str: 10, dex: 10, arm: 5}),
+      knex('robots').insert({name: 'Robot2', user_id: 2, hp: 10, str: 10, dex: 10, arm: 5}),
+      knex('robots').insert({name: 'Robot3', user_id: 3, hp: 10, str: 10, dex: 10, arm: 5}),
+    ]
+  );
+  
 };
